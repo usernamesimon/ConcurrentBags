2f8184612a7fabe40bd9b43548a8bbd15c71c2ec
salliman <sallinger99@gmail.com>
2023-06-16 16:08:05 +0200

    finally fixed(?)

--- src/concurrentBagsSimple.c
+++ src/concurrentBagsSimple.c
@@ -22,21 +22,6 @@
 #define FAO(_a, _e) atomic_fetch_or_explicit(_a, _e, memory_order_acq_rel)
 #endif
 
-#define UNMARK_MASK ~0b11
-#define MARK_BIT1 0b01
-#define MARK_BIT2 0b10
-
-#define getpointer(_markedpointer)                                             \
-  ((block_t *)(((long)_markedpointer) & UNMARK_MASK))
-#define ismarked1(_markedpointer)                                              \
-  ((((uintptr_t)_markedpointer) & MARK_BIT1) != 0x0)
-#define setmark1(_markedpointer)                                               \
-  ((block_t *)(((long)_markedpointer) | MARK_BIT1))
-#define ismarked2(_markedpointer)                                              \
-  ((((uintptr_t)_markedpointer) & MARK_BIT2) != 0x0)
-#define setmark2(_markedpointer)                                               \
-  ((block_t *)(((long)_markedpointer) | MARK_BIT2))
-
 // Initialization variables
 int Nr_threads;
 // Shared variables
@@ -47,27 +32,13 @@ bool foundAdd;
 int threadHead, stealHead, stealIndex;
 int threadID; // Unique number between 0 ... Nr_threads
 
+#pragma omp threadprivate(threadBlock, stealBlock, foundAdd, threadHead, stealHead, stealIndex, threadID)
+
 struct block_t {
   DT *_Atomic nodes[MAX_BLOCK_SIZE]; // changed void*
   long _Atomic notifyAdd[MAX_NR_THREADS / WORD_SIZE];
-  /* Attention! Also holds marked1 and marked2 in its lsb
-  Therefore have to mask when actually dereferencing the pointer
-  */
   block_t *_Atomic next;
 };
-
-void Mark1Block(block_t *block) {
-  for (;;) {
-    block_t *next = {block->next};
-    block_t *update = {next}; // TODO
-    update = setmark1(update);
-    // mark2 should have been copied
-
-    if (getpointer(next) == NULL || ismarked1(next) ||
-        CAS(&block->next, &next, update))
-      break;
-  }
-}
 
 void NotifyAll(block_t *block) {
   for (int i = 0; i < (int)(Nr_threads / WORD_SIZE); i++)
@@ -96,11 +67,6 @@ bool NotifyCheck(block_t *block, int Id) {
 }
 
 void InitBag(int num_threads) {
-  /*
-  Nr_threads = num_threads;
-  block_t* hb[Nr_threads];
-  globalHeadBlock = hb;
-  */
   Nr_threads = num_threads;
   for (int i = 0; i < Nr_threads; i++)
     globalHeadBlock[i] = (block_t *){0};
@@ -151,7 +117,6 @@ block_t *NextStealBlock(block_t *cblock) {
 
 void *TryStealBlock(int round) {
   int head = stealHead;
-  int counter = 0;
   block_t *block = stealBlock;
   foundAdd = false;
   if (block == NULL) {
@@ -167,7 +132,6 @@ void *TryStealBlock(int round) {
     stealIndex = (stealIndex + 1) % Nr_threads;
     stealHead = 0;
     stealBlock = NULL;
-    stealPrev = NULL;
     return NULL;
   }
   if (round == 1)
@@ -175,8 +139,6 @@ void *TryStealBlock(int round) {
   else if (round > 1 && NotifyCheck(block, threadID))
     foundAdd = true;
   for (;;) {
-    counter++;
-    // if (counter % 1000 == 0){printf("Thread %d possibly stuck in tryStealblock loop\n",omp_get_thread_num());}
     if (head == MAX_BLOCK_SIZE) {
       stealHead = head;
       return NULL;
@@ -196,19 +158,11 @@ void *TryRemoveAny() {
   int head = threadHead - 1;
   block_t *block = threadBlock;
   int round = 0;
-  int counter = 0;
-  for (;;) {
-    counter++;
-    // if (counter % 1000 == 0){
-    //   printf("Thread %d possibly stuck in tryRemoveAny loop\n",omp_get_thread_num());
-    //   if (block == NULL){printf("Block is NULL\n");}
-    //   else printf("Head is %d\n",head);
-    //                           }
+  for (;;) {                          
     if (block == NULL || (head < 0 && block->next == NULL)) {
       do {
         int i = 0;
         do {
-          // if (counter % 10000 == 0){printf("Thread %d possibly stuck in tryRemoveAny inner loop\n",omp_get_thread_num());}
           void *result = TryStealBlock(round);
           if (result != NULL)
             return result;
@@ -222,25 +176,25 @@ void *TryRemoveAny() {
       return NULL;
     }
     if (head < 0) {
-      block = block->next;
-      threadHead = MAX_BLOCK_SIZE;
-      head = MAX_BLOCK_SIZE;
-    }
-    DT *data = block->nodes[head];
-    if (data != NULL) {
-      if (CAS(&block->nodes[head], &data, NULL)) {
-        threadHead = head;
-        return data;
+      block = threadBlock = block->next;
+      threadHead = MAX_BLOCK_SIZE - 1;
+      head = MAX_BLOCK_SIZE - 1;
+    } else {
+      DT *data = block->nodes[head];
+      if (data != NULL) {
+        if (CAS(&block->nodes[head], &data, NULL)) {
+          threadHead = head;
+          return data;
+        }
       }
+      head--;
     }
-    head--;
   }
 }
 
+block_t *DeRefLink(struct block_t *_Atomic *link) { return (block_t *)*link; }
 
-block_t *DeRefLink(struct block_t * _Atomic* link) { return (block_t *)*link; }
-
-block_t* _Atomic NewNode(int size) {
+block_t *_Atomic NewNode(int size) {
   block_t _Atomic *new = malloc(size);
   return new;
 };
@@ -253,45 +207,11 @@ int main(int argc, char *argv[]) {
   if (argc == 2) {
     threads = (int)strtol(argv[1], NULL, 10);
   } else
-    threads = 6;
+    threads = 4;
 
   printf("Running with %d threads\r\n", threads);
   num_threads = threads;
 
-  // InitBag(threads);
-
-  // omp_set_num_threads(threads);
-
-  // tic = omp_get_wtime();
-
-  /*
-  #pragma omp parallel for
-  for (int i = 0; i < omp_get_num_threads(); i++)
-  {
-      int id = omp_get_thread_num();
-      InitThread(id);
-  }
-  for (int i = 0; i < omp_get_num_threads(); i++)
-  {
-      int id = omp_get_thread_num();
-      if (id == 0)
-      {
-          int *item = malloc(sizeof(int));
-          *item = id;
-          Add(item);
-      } else
-      {
-          int *result = (int*)TryRemoveAny();
-          if (result == NULL)
-          {
-               printf("Bag was empty\r\n");
-          } else
-          printf("Received %d\r\n", *result);
-      }
-
-
-  }
-  */
   omp_set_num_threads(num_threads);
   InitBag(num_threads);
 
@@ -300,47 +220,57 @@ int main(int argc, char *argv[]) {
   long genresult = 0;
   int multiply = 100;
   tic = omp_get_wtime();
-  #pragma omp parallel for reduction(+:genresult) private(threadBlock, stealBlock, stealPrev, foundAdd, threadHead, stealHead, stealIndex,threadID) 
-  for (int i = 0; i < omp_get_num_threads(); i++)
-  {
-      int id = omp_get_thread_num();
-      int mult = multiply;
-      long result = 0;
-      InitThread(id);
+#pragma omp parallel for reduction(+:genresult)
+  for (int i = 0; i < omp_get_num_threads(); i++) {
+    int id = omp_get_thread_num();
+    int mult = multiply;
+    long result = 0;
+    InitThread(id);
 
-      printf("Hello from thread %d\r\n", id + 1);
-      int *inc = NULL;
+    printf("Hello from thread %d\r\n", id + 1);
+    int *inc = NULL;
 
-      if (i < threads/2)
-      {
-      for (int j = 0; j < mult*(id+1); j++)
-      {
-          int *item = malloc(sizeof(int));
-          *item = id + 1;
-          Add(item);
-          // printf("Thread %d added %d\r\n", id + 1, *item);
+    if (i < threads / 2) {
+      for (int j = 0; j < mult * (id + 1); j++) {
+        int *item = malloc(sizeof(int));
+        *item = id + 1;
+        Add(item);
+        // printf("Thread %d added %d\r\n", id + 1, *item);
       }
-      } else {
-        int tries = 0;
-      do
-      {
-          inc = (int*)(TryRemoveAny());
-          if (inc != (int*)NULL) {
-              result += (long)*inc;
-              genresult += (long)*inc;
-          }
-          tries ++;
-      } while (tries < 50000);
+    } else {
+      int tries = 0;
+      do {
+        inc = (int *)(TryRemoveAny());
+        if (inc != (int *)NULL) {
+          result += (long)*inc;
+          genresult += (long)*inc;
+        }
+        tries++;
+      } while (result < threads / 2 * mult * (2) && tries < 5000);
       printf("Thread %d got result %ld\r\n", id + 1, result);
-      }
+    }
 
+    // for(int j = 0; j < mult*2; j++){
+    //   if(j< mult){
+    //     int *item = malloc(sizeof(int));
+    //     *item = 1;
+    //     Add(item);
+    //   }
+    //   else{
+    //     inc = (int *)(TryRemoveAny());
+    //     if (inc !=(int *)NULL){
+    //       genresult += (long)*inc;
+    //     }
+    //   }
+    // }
+    //  printf("Thread %d got result %ld\r\n", id + 1, genresult);
   }
   long expected = 0;
-  for (int i = 0; i < threads/2; i++)
-  {
-      expected += multiply*(i+1)*(i+1);
+  for (int i = 0; i < threads / 2; i++) {
+    expected += multiply * (i + 1) * (i + 1);
   }
-
+  // expected = multiply*threads;
+ 
   printf("Got %ld overall, from %ld possible\r\n", genresult, expected);
 
   toc = omp_get_wtime();
